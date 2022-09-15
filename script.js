4ba6fe5051158cf93e5f00e056c164ca345385e9
Adrian Lulic <adrian.lulic@gmail.com>
2022-09-15 14:13:55 +1000

    Add function to get a random Computer choice

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('Lets play Rock Paper Scissors!');
+
+function getComputerChoice() {
+  const randomNumber = Math.floor((Math.random() * 3))
+
+  switch (randomNumber) {
+    case 0:
+      return 'Rock';
+      break;
+    case 1:
+      return 'Scissors';
+      break;
+    case 2:
+      return 'Paper';
+      break;
+    default:
+      return 'Woops, error getting a computer option!'
+  }
+
+}
+

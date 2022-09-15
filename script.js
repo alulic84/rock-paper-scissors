bc5d10e135ae50a9c97160895babad34b805665e
Adrian Lulic <adrian.lulic@gmail.com>
2022-09-15 14:25:07 +1000

    Add function to play a single round

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function getComputerChoice() {
 
   switch (randomNumber) {
     case 0:
-      return 'Rock';
+      return 'rock';
       break;
     case 1:
-      return 'Scissors';
+      return 'paper';
       break;
     case 2:
-      return 'Paper';
+      return 'scissors';
       break;
     default:
       return 'Woops, error getting a computer option!'
@@ -19,3 +19,26 @@ function getComputerChoice() {
 
 }
 
+function playRound(playerSelection, computerSelection) {
+  const playerChoice = playerSelection.toLowerCase();
+
+  if (playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors') {
+    return "That's not a valid choice. Try using 'rock' 'paper' or 'scissors'.";
+  }
+
+  if (playerChoice === 'rock' && computerSelection == 'scissors')
+    return 'You Win! Rock beats Scissors.';
+  if (playerChoice === 'paper' && computerSelection == 'rock')
+    return 'You Win! Paper beats Rock.';
+  if (playerChoice === 'scissors' && computerSelection == 'paper')
+    return 'You Win! Scissors beats Paper.';
+  
+  if (playerChoice === 'rock' && computerSelection == 'paper')
+    return 'You Lose! Paper beats Rock.';
+  if (playerChoice === 'paper' && computerSelection == 'scissors')
+    return 'You Lose! Scissors beats Paper.';
+  if (playerChoice === 'scissors' && computerSelection == 'rock')
+    return 'You Lose! Rock beats Scissors.';
+
+  return 'There was no winner! Play again!'
+}
